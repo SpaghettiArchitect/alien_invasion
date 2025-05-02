dac954bcf783646a0e6e02a58f96df2670cecebf
Cris <33710623+SpaghettiArchitect@users.noreply.github.com>
2025-05-01 21:46:03 -0600

    Add clock to set framerate

--- alien_invasion.py
+++ alien_invasion.py
@@ -4,25 +4,28 @@ import pygame
 
 
 class AlienInvasion:
-    """Overall class to manage game assets and behavior."""
+    """Overall class to manage game assets and behavior"""
 
     def __init__(self):
-        """Initialize the game, and create game resorces."""
+        """Initialize the game, and create game resorces"""
         pygame.init()
+        self.clock = pygame.time.Clock()
 
         self.screen = pygame.display.set_mode((1200, 800))
         pygame.display.set_caption("Alien Invasion")
 
     def run_game(self):
-        """Start the main loop for the game."""
+        """Start the main loop for the game"""
         while True:
-            # Watch for keyboard and mouse events.
+            # Watch for keyboard and mouse events
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
 
-            # Make the most recently drawn screen visible.
+            # Make the most recently drawn screen visible
             pygame.display.flip()
+            # Sets the framerate for the game
+            self.clock.tick(60)
 
 
 if __name__ == "__main__":
