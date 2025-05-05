c1f6c5afdee9666fbfb35bc0417ce5c3fcbdcd76
Cris <33710623+SpaghettiArchitect@users.noreply.github.com>
2025-05-04 21:46:38 -0600

    Add right movement to the fleet of aliens

--- alien_invasion.py
+++ alien_invasion.py
@@ -39,6 +39,7 @@ class AlienInvasion:
             self._check_events()
             self.ship.update()
             self._update_bullets()
+            self._update_aliens()
             self._update_screen()
 
             # Sets the framerate for the game
@@ -89,6 +90,10 @@ class AlienInvasion:
         for bullet in self.bullets.copy():
             if bullet.rect.bottom <= 0:
                 self.bullets.remove(bullet)
+
+    def _update_aliens(self) -> None:
+        """Update the positions of all aliens in the fleet."""
+        self.aliens.update()
 
     def _create_fleet(self) -> None:
         """Create the fleet of aliens."""
