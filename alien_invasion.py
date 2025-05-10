9dee800589f2c1fb4756248adcfd33e44d466a58
Cris <33710623+SpaghettiArchitect@users.noreply.github.com>
2025-05-09 23:40:47 -0600

    Update scoring system
    - The score displayed on-screen updates every time the player shoot down
    an alien.
    - The points given by shooting an alien are set on the settings module.

--- alien_invasion.py
+++ alien_invasion.py
@@ -142,6 +142,10 @@ class AlienInvasion:
         # Remove any bullets and aliens that have collided
         collisions = pygame.sprite.groupcollide(self.bullets, self.aliens, True, True)
 
+        if collisions:
+            self.stats.score += self.settings.alien_points
+            self.scoreboard.prep_score()
+
         if not self.aliens:
             # Destroy existing bullets and create a new fleet
             self.bullets.empty()
